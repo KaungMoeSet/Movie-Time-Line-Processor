2a4ec78d7367effc7b82ba2e3c7e4b5ad13f8de5
KaungMoeSet <kaungmoeset@gmail.com>
2022-01-20 17:11:12 +0630

    method for minutes and sec cause these two can't have over 60

---
+++ movie-timeline/src/main/java/movie_timeline/Utils.java
@@ -0,0 +1,14 @@
+package movie_timeline;
+
+public class Utils {
+
+	public static String toDoubleDigits(int number) {
+		String result;
+		if(number > 10) 
+			result = "0%d".formatted(number);
+		else
+			result = "%d".formatted(number);
+		
+		return result;
+	}
+}
