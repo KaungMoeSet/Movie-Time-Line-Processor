1e39271d6688196405da29316d11576887d1f8f2
KaungMoeSet <kaungmoeset@gmail.com>
2022-01-20 17:04:50 +0630

    change hr,mins,sec in doConversion method and write toString

---
+++ movie-timeline/src/main/java/movie_timeline/TimeStamp.java
@@ -0,0 +1,30 @@
+package movie_timeline;
+
+public class TimeStamp {
+
+	int timeSeconds;
+	int hours, minutes, seconds;
+	
+	public TimeStamp(int timeSeconds) {
+		super();
+		this.timeSeconds = timeSeconds;
+		doConversion();
+	}
+	
+	private void doConversion() {
+		hours = timeSeconds/3600;
+		minutes = (timeSeconds - hours*3600)/60;
+		seconds = timeSeconds - hours*3600 - minutes*60;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s: %s: %s", hours, minutes, seconds);
+	}
+	
+	public static void main(String[] args) {
+		TimeStamp ts = new TimeStamp(70);
+		System.out.println(ts);
+	}
+	
+}
