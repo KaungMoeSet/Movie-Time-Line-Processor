8080171a6927ec38de65d5b57593d04ffa5c2795
KaungMoeSet <kaungmoeset@gmail.com>
2022-01-27 18:55:23 +0630

    write addMovieClip method and override toString

--- movie-timeline/src/main/java/movie_timeline/Movie.java
+++ movie-timeline/src/main/java/movie_timeline/Movie.java
@@ -25,4 +25,31 @@ public class Movie {
 		this.specifier = specifier;
 		return this;
 	}
+	
+	public Movie addMovieClip(MovieClip clip) {
+		
+		clip.setTimpeStampSpecifier(specifier);
+		
+		clip.setOffset(totalSecondsElapsed);
+		
+		totalSecondsElapsed += clip.getTotalSeconds();
+		
+		movieClips.add(clip);
+		
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder(movieClips.size());
+		String str = "MOVIE: %s".formatted(name);
+		sb.append(str).append(System.lineSeparator());
+		sb.append("*".repeat(str.length())).append(System.lineSeparator());
+		for(MovieClip clip : movieClips) {
+			sb.append(clip.toString()).append(System.lineSeparator());
+		}
+		return sb.toString();
+	}
+	
+	
 }
